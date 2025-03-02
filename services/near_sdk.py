fa6a79ce57eacb58c63f50f5d6d7393ad0e037bf
Tpavimalini <149188025+Tpavimalini@users.noreply.github.com>
2025-03-02 22:22:04 +0530

    Update near_sdk.py

--- services/near_sdk.py
+++ services/near_sdk.py
@@ -1,1 +1,8 @@
+from near_api import NearClient
+
+NEAR_RPC_URL = "https://rpc.testnet.near.org"
+client = NearClient(NEAR_RPC_URL)
+
+def call_near_contract(contract_id, method_name, args):
+    return client.call_function(contract_id, method_name, args)
 
