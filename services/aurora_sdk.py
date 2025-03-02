9f2566fd2b88856c61cbde286d2dfc1211d11e3a
Tpavimalini <149188025+Tpavimalini@users.noreply.github.com>
2025-03-02 22:20:33 +0530

    Update aurora_sdk.py

--- services/aurora_sdk.py
+++ services/aurora_sdk.py
@@ -1,1 +1,12 @@
+from web3 import Web3
+
+AURORA_RPC_URL = "https://testnet.aurora.dev"
+web3 = Web3(Web3.HTTPProvider(AURORA_RPC_URL))
+
+def get_gas_price():
+    return web3.eth.gas_price
+
+def call_contract(contract_address, abi, function_name, args):
+    contract = web3.eth.contract(address=contract_address, abi=abi)
+    return contract.functions[function_name](*args).call()
 
